14b303a22b28c47820c054371d9fcca8bd667dce
Aleeza Fatima <aleezafatima2005@gmail.com>
2026-06-18 01:40:46 +0530

    Add files via upload

---
+++ Factorial_Calculator.py
@@ -0,0 +1,17 @@
+print("FACTORIAL CALCULATOR")
+
+n=int(input("enter n="))
+
+
+if n== 0:
+    print("Factorial of 0 (0!) is 1")
+elif n<0:
+    print(" Invalid!")
+elif n>0:
+    factorial= 1
+    for i in range(1,n+1):
+        factorial=factorial * i
+    print("factorial of n is n!=", factorial)
+
+
+
